1d45ee7fa6128a73c34c962c6c67400d9394a281
Zhongrui.Yu.Linux <149889661@qq.com>
2022-09-12 21:23:33 +0200

    ch3

--- ch3/useEigen/eigenMatrix.cpp
+++ ch3/useEigen/eigenMatrix.cpp
@@ -7,17 +7,101 @@ using std::cout;
 using std::endl;
 using Eigen::Matrix;
 using Eigen::Vector3d;
+using Eigen::Matrix3d;
+using Eigen::Dynamic;
+using Eigen::MatrixXd;
+using Eigen::SelfAdjointEigenSolver;
 
 #define MATRIX_SIZE 50
 
 int main(int argc, char **argv){
   // Eigen 中所有向量和矩阵都是Eigen::Matrix，它是一个模板类。它的前三个参数为：数据类型，行，列
   // 声明一个2*3的float矩阵
-  Matrix<float, 2, 3> martrix_23;
+  Matrix<float, 2, 3> matrix_23;
 
   // 同时，Eigen 通过 typedef 提供了许多内置类型，不过底层仍是Eigen::Matrix
   // 例如 Vector3d 实质上是 Eigen::Matrix<double, 3, 1>，即三维向量
   Vector3d v_3d;
+
+  // Matrix3d 实质上是 Eigen::Matrix<double, 3, 3>
+  Matrix3d matrix_33 = Matrix3d::Zero();
+
+  // 不确定矩阵大小可以用动态矩阵
+  Matrix<double, Dynamic, Dynamic> matrix_dym;
+  // or
+  MatrixXd matrix_x;
+
+  // 初始化 输入数据
+  matrix_23 << 1, 2, 3, 4, 5, 6;
+  // 输出
+  cout << "matrix_23 from 1 to 6: \n" << matrix_23 << endl;
+
+  // 用()访问矩阵元素
+  cout << "print matrix_23:" << endl;
+  for(int i = 0; i < 2; i++){
+    for(int j = 0; j < 3; j++)
+      cout << matrix_23(i, j) << "\t";
+    cout << endl;
+  }
+
+  // 矩阵和向量相乘 
+  v_3d << 3, 2, 1;
+
+  // 但是在Eigen里你不能混合两种不同类型的矩阵，像这样是错的
+  // Matrix<double, 2, 1> result_wrong_type = matrix_23 * v_3d;
+  // 应该显式转换
+  Matrix<double, Dynamic, Dynamic> result = matrix_23.cast<double>() * v_3d;
+  cout << "[1,2,3;4,5,6]*[3,2,1] =\n" << result << endl;
+
+  // 一些矩阵运算
+  // 四则运算直接用+-*/即可
+  matrix_33 = Matrix3d::Random();      // 随机数矩阵
+  cout << "random matrix: \n" << matrix_33 << endl;
+  cout << "transpose: \n" << matrix_33.transpose() << endl;      // 转置
+  cout << "sum: " << matrix_33.sum() << endl;            // 各元素和
+  cout << "trace: " << matrix_33.trace() << endl;          // 迹
+  cout << "times 10: \n" << 10 * matrix_33 << endl;               // 数乘
+  cout << "inverse: \n" << matrix_33.inverse() << endl;        // 逆
+  cout << "det: " << matrix_33.determinant() << endl;    // 行列式
+
+  // 特征值
+  SelfAdjointEigenSolver<Matrix3d> eigen_solver(matrix_33.transpose() * matrix_33);
+  cout << "Eigen values = \n" << eigen_solver.eigenvalues() << endl;
+  cout << "Eigen vectors = \n" << eigen_solver.eigenvectors() << endl; // 每列是一个eigen vector
+
+
+  // 解方程
+  // 我们求解 matrix_NN * x = v_Nd 这个方程
+  // N的大小在前边的宏里定义，它由随机数生成
+  // 直接求逆自然是最直接的，但是求逆运算量大
+
+  Matrix<double, MATRIX_SIZE, MATRIX_SIZE> matrix_NN
+      = MatrixXd::Random(MATRIX_SIZE, MATRIX_SIZE);
+  matrix_NN = matrix_NN * matrix_NN.transpose();  // 保证半正定
+  Matrix<double, MATRIX_SIZE, 1> v_Nd = MatrixXd::Random(MATRIX_SIZE, 1);
+
+  clock_t time_stt = clock(); // 计时
+  // 直接求逆
+  Matrix<double, MATRIX_SIZE, 1> x = matrix_NN.inverse() * v_Nd;
+  cout << "time of normal inverse is "
+       << 1000 * (clock() - time_stt) / (double) CLOCKS_PER_SEC << "ms" << endl;
+  cout << "x = " << x.transpose() << endl;
+
+  // 通常用矩阵分解来求，例如QR分解，速度会快很多
+  time_stt = clock();
+  x = matrix_NN.colPivHouseholderQr().solve(v_Nd);
+  cout << "time of Qr decomposition is "
+       << 1000 * (clock() - time_stt) / (double) CLOCKS_PER_SEC << "ms" << endl;
+  cout << "x = " << x.transpose() << endl;
+
+  // 对于正定矩阵，还可以用cholesky分解来解方程
+  time_stt = clock();
+  x = matrix_NN.ldlt().solve(v_Nd);
+  cout << "time of ldlt decomposition is "
+       << 1000 * (clock() - time_stt) / (double) CLOCKS_PER_SEC << "ms" << endl;
+  cout << "x = " << x.transpose() << endl;
+
+  cout << "End" << endl;
     
     return 0;
 }
