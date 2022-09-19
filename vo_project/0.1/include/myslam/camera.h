1b524cf01ae830e07dc53e177792bc5f46d5fa08
Zhongrui.Yu.Linux <149889661@qq.com>
2022-09-19 22:38:45 +0200

    fixed typo

--- vo_project/0.1/include/myslam/camera.h
+++ vo_project/0.1/include/myslam/camera.h
@@ -9,7 +9,7 @@ namespace myslam
     {
         public: 
 
-        typedef std::share_ptr<Camera> Ptr;
+        typedef std::shared_ptr<Camera> Ptr;
         float fx_, fy_, cx_, cy_, depth_scale_;
 
         Camera();
@@ -23,7 +23,7 @@ namespace myslam
         Vector2d camera2pixel( const Vector3d& p_c );
         Vector3d pixel2camera( const Vector2d& p_p, double depth = 2 );
         Vector3d pixel2world( const Vector2d& p_p, const SE3& T_c_w, double depth = 1 );
-        Vector2d world2pixe( const Vector3d& p_w, const SE3& T_c_w );
+        Vector2d world2pixel( const Vector3d& p_w, const SE3& T_c_w );
 
 
     };
