49fa748ac93947c4f9b8655eb88510fe6e0b7a51
yuwsl <149889661@qq.com>
2022-09-27 20:40:40 +0200

    class map

---
+++ vo_project/0.1/include/myslam/map.h
@@ -0,0 +1,26 @@
+// map类中储存了各个关键帧和路标点，既需要随机访问，又需要随时插入和删除
+#ifndef MAP_H
+#define MAP_H
+
+#include "myslam/common_include.h"
+#include "myslam/frame.h"
+#include "myslam/mappoint.h"
+
+namespace myslam
+{
+    class Map
+    {
+    public:
+        typedef shared_ptr<Map> Ptr;
+        unordered_map<unsigned long, MapPoint::Ptr> map_points_; //all landmarks
+        unordered_map<unsigned long. Frame::Ptr> keyframes_;    // all key-frames
+
+        Map() {}
+
+        void insertKeyFrame(Frame::Ptr frame);
+        void insertMapPoint(MapPoint::Ptr map_point);
+        
+    };
+}
+
+#endif
